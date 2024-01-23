38d271c18006bb07b4617b7f69680fd0790af779
BalakinMike <mikb2220@gmail.com>
2024-01-23 15:08:02 +0300

    released closing ImageViewer by Alt_F4 keys

--- photo_viewer.py
+++ photo_viewer.py
@@ -15,6 +15,8 @@ def key_emulation(root, files):
     for i in range(len(files)):
         time.sleep(5)
         keyboard.press(Key.right)
+    with keyboard.pressed(Key.alt_l):
+        keyboard.press(Key.f4)
 
 root_directory = r"D:\ProjectPython\My_Projects\photo_frame\input"
 for root, directories, files in os.walk(root_directory):
@@ -24,5 +26,6 @@ for root, directories, files in os.walk(root_directory):
             key_emulation(root, files)
             
             break
-    subprocess.call(["taskkill", "/F", "/IM", "C:\Program Files\HONOR\PCManager\HnPhotoViewer.exe"])
+    
+# subprocess.call(["taskkill", "/F", "/IM", "C:\Program Files\HONOR\PCManager\HnPhotoViewer.exe"])
 #"C:\Program Files\HONOR\PCManager\HnPhotoViewer.exe"
